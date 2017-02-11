71111440f194bcccfb1e50364bcc6d9ace188735
Benjamin Oakes <hello@benjaminoakes.com>
2017-02-10 19:34:17 -0600

    Minify Javascript on build

--- config.rb
+++ config.rb
@@ -132,11 +132,9 @@ end
 # Build-specific configuration
 configure :build do
   activate :minify_css
+  activate :minify_javascript
 
   activate :google_analytics do |ga|
     ga.tracking_id = 'UA-91743483-1'
   end
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
