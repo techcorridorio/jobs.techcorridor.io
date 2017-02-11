edfb955bc668f5b26e1f4f6781d69be3c77687d3
Benjamin Oakes <hello@benjaminoakes.com>
2017-02-11 17:38:22 -0600

    Refactor: Add module (JobEngine)

--- config.rb
+++ config.rb
@@ -30,157 +30,159 @@ end
 # Helpers
 ###
 
-require 'uri'
+module JobEngine
+  require 'uri'
 
-class Position
-  def self.null
-    new({})
-  end
+  class Position
+    def self.null
+      new({})
+    end
 
-  attr_reader :path, :updated_at, :source_url, :featured, :title, :type, :company, :company_url, :location
+    attr_reader :path, :updated_at, :source_url, :featured, :title, :type, :company, :company_url, :location
 
-  def initialize(data)
-    @path = data[:path]
-    @updated_at = data[:updated_at]
-    @source_url = data[:source_url]
-    @featured = data[:featured]
-    @title = data[:title]
-    @type = data[:type]
-    @company = data[:company]
-    @company_url = data[:company_url]
-    @location = data[:location]
-  end
+    def initialize(data)
+      @path = data[:path]
+      @updated_at = data[:updated_at]
+      @source_url = data[:source_url]
+      @featured = data[:featured]
+      @title = data[:title]
+      @type = data[:type]
+      @company = data[:company]
+      @company_url = data[:company_url]
+      @location = data[:location]
+    end
 
-  def guid
-    @guid ||= File.basename(path).split('.').first
-  end
+    def guid
+      @guid ||= File.basename(path).split('.').first
+    end
 
-  def page_title
-    if title && company
-      "#{title} at #{company}"
+    def page_title
+      if title && company
+        "#{title} at #{company}"
+      end
+    end
+
+    def featured?
+      !!@featured
+    end
+
+    def absolute_path
+      "/#@path"
+    end
+
+    def has_source_url?
+      !!@source_url
+    end
+
+    def source_hostname
+      if has_source_url?
+        uri = URI.parse(@source_url)
+        uri.hostname
+      end
     end
   end
 
-  def featured?
-    !!@featured
-  end
-
-  def absolute_path
-    "/#@path"
-  end
-
-  def has_source_url?
-    !!@source_url
-  end
-
-  def source_hostname
-    if has_source_url?
-      uri = URI.parse(@source_url)
-      uri.hostname
+  class PositionFactory
+    def initialize(resource)
+      @resource = resource
     end
-  end
-end
 
-class PositionFactory
-  def initialize(resource)
-    @resource = resource
-  end
+    def position
+      if @resource.data[:position]
+        data = @resource.
+          data[:position].
+          merge(extended_data)
 
-  def position
-    if @resource.data[:position]
-      data = @resource.
-        data[:position].
-        merge(extended_data)
+        Position.new(data)
+      else
+        Position.null
+      end
+    end
 
-      Position.new(data)
-    else
-      Position.null
+    private
+
+    def extended_data
+      {
+        path: @resource.path,
+        updated_at: updated_at,
+      }
+    end
+
+    def updated_at
+      File.mtime(@resource.source_file)
     end
   end
 
-  private
+  class PositionCollectionFactory
+    def initialize(resources)
+      @resources = resources
+    end
 
-  def extended_data
-    {
-      path: @resource.path,
-      updated_at: updated_at,
-    }
+    def position_collection
+      positions = @resources.
+        select { |resource| resource.data[:position] }.
+        map { |resource| PositionFactory.new(resource).position }
+
+      PositionCollection.new(positions)
+    end
   end
 
-  def updated_at
-    File.mtime(@resource.source_file)
-  end
-end
+  require 'forwardable'
 
-class PositionCollectionFactory
-  def initialize(resources)
-    @resources = resources
-  end
+  class PositionCollection
+    extend Forwardable
+    include Enumerable
 
-  def position_collection
-    positions = @resources.
-      select { |resource| resource.data[:position] }.
-      map { |resource| PositionFactory.new(resource).position }
+    def_delegators :@positions, :each, :empty?
 
-    PositionCollection.new(positions)
-  end
-end
+    attr_reader :positions
 
-require 'forwardable'
+    def initialize(positions)
+      @positions = positions
+    end
 
-class PositionCollection
-  extend Forwardable
-  include Enumerable
+    def count
+      @positions.length
+    end
 
-  def_delegators :@positions, :each, :empty?
+    def reverse_chronological
+      wrap(@positions.sort_by { |position| position.updated_at }.reverse)
+    end
 
-  attr_reader :positions
+    def featured
+      wrap(@positions.select { |position| position.featured? })
+    end
 
-  def initialize(positions)
-    @positions = positions
-  end
+    private
 
-  def count
-    @positions.length
+    def wrap(array)
+      self.class.new(array)
+    end
   end
 
-  def reverse_chronological
-    wrap(@positions.sort_by { |position| position.updated_at }.reverse)
+  class JobEngine
+    def initialize(context)
+      @context = context
+    end
+
+    def position
+      PositionFactory.new(@context.current_page).position
+    end
+
+    def positions
+      PositionCollectionFactory.new(@context.sitemap.resources).position_collection
+    end
   end
 
-  def featured
-    wrap(@positions.select { |position| position.featured? })
-  end
-
-  private
-
-  def wrap(array)
-    self.class.new(array)
-  end
-end
-
-class JobEngine
-  def initialize(context)
-    @context = context
-  end
-
-  def position
-    PositionFactory.new(@context.current_page).position
-  end
-
-  def positions
-    PositionCollectionFactory.new(@context.sitemap.resources).position_collection
-  end
-end
-
-module JobEngineHelpers
-  def job_engine
-    JobEngine.new(self)
+  module JobEngineHelpers
+    def job_engine
+      JobEngine.new(self)
+    end
   end
 end
 
 # Methods defined in the helpers block are available in templates
-helpers JobEngineHelpers
+helpers JobEngine::JobEngineHelpers
 
 configure :development do
   activate :google_analytics do |ga|
