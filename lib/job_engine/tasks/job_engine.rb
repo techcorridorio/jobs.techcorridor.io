a756ea1b60c56d975b2d659ba85efd7a9cffe9dd
Benjamin Oakes <hello@benjaminoakes.com>
2017-02-15 20:38:07 -0600

    Move linkchecker to Rake task

--- lib/job_engine/tasks/job_engine.rb
+++ lib/job_engine/tasks/job_engine.rb
@@ -5,4 +5,10 @@ namespace :job_engine do
   task :clean do
     FileUtils.rm_rf('build', verbose: true)
   end
+
+  desc 'Check links in built site'
+  task :check_links do
+    sh 'middleman build --verbose'
+    sh 'linkchecker "build/"'
+  end
 end
